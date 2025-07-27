8795496eab3ae6c741e06142bb878dadec8614db
Sathvik-Murarishetty <sathvikmurarishetty18@gmail.com>
2025-07-27 13:09:05 +0530

    +

--- components/ui/card-stack.tsx
+++ components/ui/card-stack.tsx
@@ -40,12 +40,12 @@ export const CardStack = ({
     };
 
     return (
-        <div className="relative  h-60 w-60 md:h-60 md:w-96">
+        <div className="relative h-60 w-84 md:h-60 md:w-96">
             {cards.map((card, index) => {
                 return (
                     <motion.div
                         key={card.id}
-                        className="absolute bg-white h-60 w-60 md:h-60 md:w-96 rounded-3xl p-4 shadow-xl border border-[var(--accent-sage)] shadow-black/[0.1] flex flex-col justify-between"
+                        className="absolute bg-white h-60 w-84 md:h-60 md:w-96 rounded-3xl p-4 shadow-xl border border-[var(--accent-sage)] shadow-black/[0.1] flex flex-col justify-between"
                         style={{
                             transformOrigin: "top center",
                         }}
