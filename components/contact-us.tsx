4b694585b13ddc74cf7f48f9282a12ab85a7c54f
Sathvik-Murarishetty <sathvikmurarishetty18@gmail.com>
2025-07-27 11:51:37 +0530

    +

--- components/contact-us.tsx
+++ components/contact-us.tsx
@@ -10,7 +10,7 @@ export function ContactUs() {
         console.log("Form submitted");
     };
     return (
-            <section className="mx-auto max-w-7xl px-6 py-20 flex flex-col-reverse lg:flex-row gap-8 items-center lg:items-start text-center lg:text-left">
+            <section className="mx-auto max-w-7xl px-6 py-20 flex flex-col lg:flex-row gap-8 items-center lg:items-start text-center lg:text-left">
             
                         <div className="w-full lg:w-1/2 space-y-6">
                             <h2 className="text-5xl font-semibold font-serif leading-tight text-[var(--foreground)] sm:text-5xl md:text-6xl">
--- components/services.tsx
+++ components/services.tsx
@@ -187,7 +187,7 @@ export function TabsServices() {
     ];
 
     return (
-        <div className="h-[20rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full items-start justify-start my-10">
+        <div className="h-[40rem] md:h-[40rem] [perspective:1000px] relative b flex flex-col max-w-5xl mx-auto w-full items-start justify-start my-10">
             <Tabs tabs={tabs} />
         </div>
     );
