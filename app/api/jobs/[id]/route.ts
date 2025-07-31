5803a39113d9e20715f7626fad3f5a4a84d83e3f
Sathvik-Murarishetty <sathvikmurarishetty18@gmail.com>
2025-07-31 16:49:01 +0530

    +

--- app/api/jobs/[id]/route.ts
+++ app/api/jobs/[id]/route.ts
@@ -1,11 +1,8 @@
 import { NextResponse } from 'next/server';
 import { sql } from '@vercel/postgres';
 
-export async function GET(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
-  const { id } = params;
+export async function GET(req: Request, context: any) {
+  const id = context.params.id;
 
   const { rows } = await sql`SELECT * FROM jobs WHERE id = ${id}`;
   if (rows.length === 0) {
